a6258adc75e0dd819da9bd53375c9a82dda53c94
Brittani <brittani.morganfield@gmail.com>
2021-08-05 21:59:20 -0500

    work

---
+++ main.js
@@ -0,0 +1,17 @@
+// ask date
+function askDateTime(){
+   var askDate = prompt("Enter date please");
+console.log(askDate);
+
+// ask name
+let askName = prompt("What's your name?");
+console.log(askName)
+
+// ask confidence level
+let askCon = prompt("What's your confidence level?");
+console.log(askCon)
+
+// ask entry
+let askEntry = prompt("Enter your thoughts!");
+console.log(askEntry);
+}
